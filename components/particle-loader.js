42e8e048ff0cd18aae6429f030f8e661d1d0df63
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-21 21:01:33 -0400

    Update particle-loader.js

--- components/particle-loader.js
+++ components/particle-loader.js
@@ -14,8 +14,6 @@ export const ParticleContainer = forwardRef(({children}, ref) => (
             className="particle"
             m="auto"
             alignItems='center'
-            mt={['-200px', '-160px', '-420px']}
-            mb={['-300px', '-200px', '-200px']}
             w={[280, 480, 640]}
             h={[280, 480, 640]}
             position="relative"
